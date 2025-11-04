0c9a91454ea52f720ec76c5d7d0aa01fe4690c45
Eduard197 <oeduard906@gmail.com>
2025-11-04 18:22:49 +0200

    Створив папку Homework1 і до неї додав index.html, main.js виконав завдання.

---
+++ Homework1/main.js
@@ -0,0 +1,44 @@
+let a='hello'
+console.log(a)
+let b='owu'
+console.log(b)
+let c='com'
+console.log(c)
+let d='ua'
+console.log(d)
+let f =1;
+console.log(f)
+let e =10;
+console.log(e)
+let j =-999;
+console.log(e)
+let l =123;
+console.log(l)
+let m =3.14;
+console.log(m)
+let o =2.7;
+console.log(o)
+let p =16;
+console.log(p)
+let r =true;
+console.log(r)
+let s =false;
+console.log(s)
+const firstName ='Едуард'
+const middleName ='Миколайович'
+const lastName ='Орлов'
+let msg = `${firstName} ${middleName} ${lastName}`;
+console.log(msg)
+let t = 100;
+let u =`100`;
+let v =true;
+console.log(typeof t);
+console.log(typeof u);
+console.log(typeof v);
+const userName = prompt(`Будь ласка, введіть Ваше Ім'я:`);
+const userMiddle = prompt(`Будь ласка, введіть Ваше По-батькові:`);
+const userAge = prompt(`Будь ласка, введіть скільки Вам років`);
+console.log(userName);
+console.log(userMiddle);
+console.log(userAge);
+
